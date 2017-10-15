84d71ecae881eee1a8a16a4ddd4e6b4a4bfb8aff
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2017-10-15 17:16:41 +0200

    Initialize project

---
+++ BagiraDanceStudio.Web/BagiraDanceStudio.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BagiraDanceStudio.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
